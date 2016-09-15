caa50fc651c066994bc04661a573fe7ccda5619c
BakrAldulaimi <bakr-kamil@hotmail.com>
2016-09-16 01:26:30 +0200

    dd

---
+++ app/src/main/java/com/example/tvshow/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tvshow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//bbb
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
